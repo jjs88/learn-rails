ca07995cfa1f526e38adc541c216e1b292bc613d
jjs88 <jjs457@gmail.com>
2013-12-22 16:09:58 -0500

    added mailer list for MailChimp

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 LearnRails::Application.routes.draw do 
   #goes to visitors new controller
   resources :contacts, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
 end
